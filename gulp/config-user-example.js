ebfbf2df24ddf51f84a5db89bd5aefa87c8b490c
René Stalder <rene@whatwedo.ch>
2015-02-10 11:52:21 +0100

    docs: add user config example
    I've added an example of a user config we often use in our local environments.

---
+++ gulp/config-user-example.js
@@ -0,0 +1,22 @@
+/* This is an exmaple of a user config which is for use in your local environment only.
+*  You can rename this to config-user.js and the parameters of the dev config will be overridden.
+*  The config-user.js will not be added to the repository since it's not for your team.
+*
+*  In this example we configured browsersync to point to our local Apache Server. You can overwrite any
+*  Parameter from the dev config.
+*/
+
+var dest = "./dist";
+var src = './src';
+
+module.exports = {
+  browserSync: {
+    server: false,
+    files: [
+      dest + "/**",
+      "!" + dest + "/**.map"
+    ],
+    proxy: "myapp.dev",
+    open: false
+  }
+};
